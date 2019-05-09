512c1ae19b6032cacc5a47992294f6de0b095b34
Andre Dupin <dd.dupin@gmail.com>
2019-05-09 14:44:55 +0200

    questionnaire fait

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,54 +11,56 @@ namespace App\Controller;
 class HomeController extends AbstractController
 {
 
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
     public function index()
     {
-        if (!isset($_SESSION)) {
+        if (empty($_SESSION)) {
             session_start();
         }
 
-        {
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $_SESSION['mdp'][] = $_POST['loginPassword'];
-                $_SESSION['errors'][] = 'essai';
-                if (isset($_SESSION['mdp'][2])) {
-                    header('location:/Home/wcs');
-                }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_SESSION['mdp'][] = $_POST['loginPassword'];
+            $_SESSION['errors'][] = 'essai';
+            if (isset($_SESSION['mdp'][2])) {
+                header('location:/Home/wcs');
             }
-            return $this->twig->render('Home/index.html.twig', ['session' => $_SESSION,]);
         }
+        return $this->twig->render('Home/index.html.twig', ['session' => $_SESSION,]);
     }
 
 
     public function wcs()
-
     {
-        if (!isset($_SESSION)) {
+        if (empty($_SESSION)) {
             session_start();
         }
-        if (!isset($_SESSION['mdp'][2])) {
+        if (empty($_SESSION['mdp'][2])) {
             header('location:/Home/index');
         }
         return $this->twig->render('Home/wcs.html.twig', ['session' => $_SESSION,]);
     }
 
     public function quizz()
-
     {
-        if (!isset($_SESSION)) {
+        if (empty($_SESSION)) {
             session_start();
         }
-        if (!isset($_SESSION['mdp'][2])) {
+        if (empty($_SESSION['mdp'][2])) {
             header('location:/Home/index');
         }
         return $this->twig->render('Home/kahoot.html.twig', ['session' => $_SESSION,]);
+    }
+
+    public function questions()
+    {
+        if (empty($_SESSION)) {
+            session_start();
+        }
+        if (empty($_SESSION['mdp'][2])) {
+            header('location:/Home/index');
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo 'OK';
+        }
+        return $this->twig->render('Home/questions.html.twig', ['session' => $_SESSION,]);
     }
 }
