f81af32b64163a783bbdfe4c2bf8ca7e6a660d9d
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-10 10:18:55 +0200

    Forme du form

--- public/assets/js/counter.js
+++ public/assets/js/counter.js
@@ -2,10 +2,21 @@ $('.counter-count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 30000,
+        duration: 50000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
     });
 });
+
+var i = 0;
+function makeProgress(){
+    if(i < 100){
+        i = i + 1;
+        $(".progress-bar").css("width", i + "%").text(i + " PR acceptés par Sylvain");
+    }
+    // Wait for sometime before running this script again
+    setTimeout("makeProgress()", 3000);
+}
+makeProgress();
